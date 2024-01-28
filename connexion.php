ff24b6f20a593346b474880a6045a0bf69619e42
CIROY <danielciroy87@gmail.com>
2024-01-28 16:59:09 +0200

    suit gestion des employes

---
+++ connexion.php
@@ -0,0 +1,11 @@
+<?php
+
+//connexion ala bd
+$con=mysqli_connect("localhost","root","","entreprise");
+if(!$con){
+    echo'veuillez vous connecter';
+}
+
+
+
+?>
--- index.php
+++ index.php
@@ -12,7 +12,10 @@
     <div class="container">
         <a href="ajouter.php" class="Btn_add"><img src="images/plus.png" alt="ajouter">Ajouter</a>
 
+
         <table>
+            
+     
             <tr id="items">
                 <th>Nom</th>
                 <th>Prenom</th>
@@ -20,20 +23,33 @@
                 <th>Modifier</th>
                 <th>Supprimer</th>
             </tr>
-            <tr>
-                <td>steve</td>
-                <td>Marc</td>
-                <td>20 ans</td>
-                <td><a href="modifier.html"><img src="images/pen.png" alt=""></a></td>
-                <td><a href="#"><img src="images/trash.png" alt=""></a></td>
-            </tr>
-            <tr>
-                <td>dan</td>
-                <td>ciroy</td>
-                <td>22 ans</td>
-                <td><a href="modifier.php"><img src="images/pen.png" alt=""></a></td>
-                <td><a href="#"><img src="images/trash.png" alt=""></a></td>
-            </tr>
+            <?php
+        // inclure la page de connexion a la base de donnee
+        include_once "connexion.php";
+        
+        //requetes pour afficher les employes
+        $req=mysqli_query($con,"SELECT * FROM employes");
+        if(mysqli_num_rows($req)==0){
+            //s'il n'existe pas d'employees dans la base de donnee , on affiche ce message:
+            echo"il n'ya pas encore d'employes";
+        }else{
+            //sinon, on affiche les employes qui sonts dans la base de donnee
+
+            while($row=mysqli_fetch_assoc($req)){
+                 ?>
+                 <tr>
+                    <td><?=$row['nom']?></td>
+                    <td><?=$row['prenom']?></td>
+                    <td><?=$row['age']?></td>
+                    <!-- nous allons mettre l'id des employes dans ces lien-->
+                    <td><a href="modifier.php?id=<?=$row['id']?>"><img src="images/pen.png" alt=""></a></td>
+                    <td><a href="supprimer.php?id=<?=$row['id']?>"><img src="images/trash.png" ></a></td>
+                 </tr>
+               <?php
+            }
+        }
+        ?>
+         
         </table>
     </div>
 
